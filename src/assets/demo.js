f2ba79f86e6fbf4c03a79753aa606e0b703fdc8d
Rohit Khati <50332813+ro-hit81@users.noreply.github.com>
2020-09-05 16:27:12 +0545

    Completed Initial task of Choropleth function.

--- src/assets/demo.js
+++
@@ -1,5 +0,0 @@
-const data = [1, 2, 3, 4, 5, 6]
-
-const new_data = data.splice(1, 1, "Rohit")
-
-console.log(data)
--- src/components/maps/ProvinceDensity.vue
+++ src/components/maps/ProvinceDensity.vue
@@ -5,22 +5,35 @@
                 :zoom= "zoom"
                 :center= "center"
                 :bounds= "bounds"
-                style= "z-index:0; height:75vh;"
+                style= "z-index:0; height:75vh; background-color: #64B5F6;"
+                @update:zoom= "zoomUpdate"
+                @update:center= "centerUpdate"
                 :options="mapOptions"
             >
-            <l-choropleth-layer
-                    :data="coronaData"
-                    titleKey="province"
-                    idKey="province"
-                    :value="value"
-                    geojsonIdKey="PROVINCE"
-                    :geojson="ProvinceJSON"
-                    :colorScale="colorScale"
-            >
+                <l-choropleth-layer
+                        :data="coronaData"
+                        titleKey="province"
+                        idKey="province_id"
+                        :value="value"
+                        :extraValues="extraValues"
+                        geojsonIdKey="PROVINCE"
+                        :geojson="ProvinceJSON"
+                        :colorScale="colorScale"
+                >
 
-            </l-choropleth-layer>
+                </l-choropleth-layer>
             </l-map>
         </div>
+        <v-row class="text-center overline">
+            <v-col>
+                <span class="font-weight-bold grey--text"> Current Zoom Level:</span>
+                <span class="font-weight-medium"> {{zoom}}</span>
+            </v-col>
+            <v-col>
+                <span class="font-weight-bold grey--text">Map Center: </span>
+                <span class="font-weight-medium">{{center.lat}}&deg;, {{center.lng}}&deg;</span>
+            </v-col>
+        </v-row>
     </v-col>
 </template>
 
@@ -29,6 +42,7 @@ import axios from 'axios'
 import L from 'leaflet'
 import {LMap} from 'vue2-leaflet'
 import {ChoroplethLayer} from 'vue-choropleth'
+// import {demo} from '../../assets/demo'
 
 import ProvinceJSON from '@/assets/Province'
 
@@ -38,7 +52,7 @@ axios.get('https://data.nepalcorona.info/api/v1/covid').then((res) => {
   const groups= array.reduce((acc, arr) => {
       const province = arr.province
       if(!acc[province]) {
-          acc[province]=[]
+          acc[province]= []
       }
       acc[province].push(arr)
       return acc
@@ -46,10 +60,11 @@ axios.get('https://data.nepalcorona.info/api/v1/covid').then((res) => {
   const groupingProvince = Object.keys(groups).map((province) => {
       return {
           province: province,
-          infected: groups[province].length
+          province_id: parseInt(province),
+          infected: groups[province].length,
       }
   })
-  coronaData.push(groupingProvince)
+  coronaData.push(...groupingProvince)
 })
 
 export default {
@@ -59,31 +74,49 @@ export default {
     },
     data() {
         return {
+            coronaData,
             zoom: 7, 
-            center: L.latLng(28.197842, 84.528289),
+            center: L.latLng(28.4493819395240528, 84.09959913133743),
             bounds: [],
             ProvinceJSON,
-            colorScale: ["e7d090", "e9ae7b", "de7062"],
+            colorScale: ["#4A148C", "#2979FF", "#651FFF", '#558B2F', '#00C853', '#E65100', '#FFEE58'],
             mapOptions: {
                 attributionControl: false
             },
-            coronaData,
             value: {
                 key: "infected",
-                metric: "number of infected"
-            }
+                metric: "people"
+            },
+            extraValues: [
+                {
+                    key: 'infected',
+                    metric: "people"
+                }
+            ]
         }
     },
     methods: {
-        // zoomUpdate(zoom) {
-        //     return this.zoom = zoom
-        // },
-        // centerUpdate(center) {
-        //     return this.center = center
-        // },
+        zoomUpdate(zoom) {
+            return this.zoom = zoom
+        },
+        centerUpdate(center) {
+            return this.center = center
+        },
         latLng: function(lat, lng) {
             return L.latLng(lat, lng)
         }
+    },
+    mounted() {
+        this.$root.$on('clicked-province', (ProvinceData) => {
+            let polygon = L.polygon(ProvinceData.geometry.coordinates);
+            let bounds = polygon.getBounds();
+            let southWest = bounds.getSouthWest();
+            let northEast = bounds.getNorthEast();
+            let cSouthWest = L.latLng(southWest.lng, southWest.lat);
+            let cNortEast = L.latLng(northEast.lng, northEast.lat);
+            let newBounds = L.latLngBounds(cSouthWest, cNortEast);
+            this.bounds = newBounds
+        });
     }
 }
 </script>
@@ -91,7 +124,7 @@ export default {
 <style scoped>
 @import "~leaflet/dist/leaflet.css";
 
-.map{
+#map{
     height: 75vh;
 }
 </style>
